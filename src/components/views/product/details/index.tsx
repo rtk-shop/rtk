f3b2e2b48d54829ac49b138a4bb4bc88a1eaa9c6
fresonn <vladislavs319@gmail.com>
2024-07-15 13:56:55 +0300

    /product: price styles update

--- src/components/views/product/details/index.tsx
+++ src/components/views/product/details/index.tsx
@@ -53,66 +53,71 @@ export function Details({ id, sku, title, currentPrice, tags, inStock, basePrice
   return (
     <section className={styles.container}>
       <h1 className={styles.title}>{title}</h1>
-      <div className={styles.availability}>
-        <div className={clsx(styles.availabilityPlug, !inStock && styles.availabilityOutStock)}>
-          <div
-            className={clsx({
-              ['svg-icon']: true,
-              [styles.availabilityIcon]: true,
-              [styles.availabilityIconInStock]: inStock
-            })}
-          >
-            {inStock ? <CheckIcon /> : <ExclamationIcon />}
-          </div>
-          <span>{inStock ? 'В наличии' : 'Нет в наличии'}</span>
-        </div>
-        <div className={styles.inner}>
-          <p className={styles.skuCode}>
-            <span>Код:</span>&nbsp;{sku}
-          </p>
-        </div>
-      </div>
-      {/*  */}
-      {basePrice !== currentPrice && (
-        <div className={styles.discountPriceBox}>
-          <span className={styles.discountPrice}>{formatPrice(basePrice)}&nbsp;₴</span>
-          <span className={styles.percentage}>
-            -{Math.round(((basePrice - currentPrice) * 100) / basePrice)}%
-          </span>
-        </div>
-      )}
-      <p className={styles.currentPrice}>
-        {formatPrice(currentPrice)}&nbsp;<span>₴</span>
-      </p>
-      {/*  */}
-      <SizeGuide current="L" available={['M', '2XL', 'L']} />
-      <div className={styles.buttonsWrapper}>
-        <Button
-          fullWidth
-          color="secondary"
-          disabled={!inStock}
-          onClick={handleOrderNow}
-          className={styles.orderNowButton}
-        >
-          Купить сейчас
-        </Button>
-        <Button
-          fullWidth
-          color="secondary"
-          onClick={handleAddToCart}
-          className={styles.orderButton}
-          startIcon={
-            <SvgIcon className={styles.orderButtonIcon}>
-              <HeaderCartIcon />
+      <div className={styles.inner}>
+        <div className={styles.availability}>
+          <div className={clsx(styles.availabilityPlug, !inStock && styles.availabilityOutStock)}>
+            <SvgIcon
+              className={clsx({
+                [styles.availabilityIcon]: true,
+                [styles.availabilityIconInStock]: inStock
+              })}
+            >
+              {inStock ? <CheckIcon /> : <ExclamationIcon />}
             </SvgIcon>
-          }
-        >
-          {inStock ? 'Добавить в корзину' : 'Сообщить когда появиться'}
-        </Button>
+            <span>{inStock ? 'В наличии' : 'Нет в наличии'}</span>
+          </div>
+          <div className={styles.skuWrapper}>
+            <p className={styles.skuCode}>
+              <span>Код:</span>&nbsp;{sku}
+            </p>
+          </div>
+        </div>
+
+        {/*  */}
+        <div className={styles.priceBox}>
+          <p className={styles.currentPrice}>
+            {formatPrice(currentPrice)}&nbsp;<span>$</span>
+          </p>
+          {basePrice !== currentPrice && (
+            <div className={styles.discountPriceBox}>
+              <span className={styles.discountPrice}>{formatPrice(basePrice)}&nbsp;$</span>
+              <span className={styles.percentage}>
+                -{Math.round(((basePrice - currentPrice) * 100) / basePrice)}%
+              </span>
+            </div>
+          )}
+        </div>
+        {/*  */}
+        <SizeGuide current="L" available={['M', '2XL', 'L']} />
+        <div className={styles.buttonsWrapper}>
+          <Button
+            fullWidth
+            color="secondary"
+            disabled={!inStock}
+            onClick={handleOrderNow}
+            className={styles.orderNowButton}
+          >
+            Купить сейчас
+          </Button>
+          <Button
+            fullWidth
+            color="secondary"
+            onClick={handleAddToCart}
+            className={styles.orderButton}
+            disabled={!inStock}
+            startIcon={
+              <SvgIcon className={styles.orderButtonIcon}>
+                <HeaderCartIcon />
+              </SvgIcon>
+            }
+          >
+            {inStock ? 'Добавить в корзину' : 'Сообщить когда появиться'}
+          </Button>
+        </div>
+        <SubControls productId={id} />
+        {tags && tags.length > 1 && <Tags tags={tags} />}
+        <Delivery />
       </div>
-      <SubControls productId={id} />
-      {tags && tags.length > 1 && <Tags tags={tags} />}
-      <Delivery />
     </section>
   )
 }
