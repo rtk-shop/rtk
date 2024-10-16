21ed0b00952fcac8d4070b2d42d443866a00cb9b
fresonn <vladislavs319@gmail.com>
2024-10-16 18:03:32 +0300

    ui: badge styled with TW

--- src/components/ui/badge/index.tsx
+++ src/components/ui/badge/index.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from 'react'
-import clsx from 'clsx'
-import styles from './styles.module.scss'
+import { cva } from 'cva'
 
 interface BadgeProps {
   max?: number
@@ -10,6 +9,20 @@ interface BadgeProps {
   className?: string
   dotClassName?: string
 }
+
+const container = cva('inline-flex relative shrink-0 align-middle')
+
+const dot = cva(
+  'text-white size-5 flex z-10 px-1.5 absolute flex-wrap rounded-full items-center font-medium text-xs leading-none content-center justify-center top-0 right-0 flex-row origin-center transition-transform',
+  {
+    variants: {
+      intent: {
+        in: ['scale-100', 'translate-x-2/4', '-translate-y-2/4'],
+        out: ['scale-0', 'translate-x-2/4', '-translate-y-2/4']
+      }
+    }
+  }
+)
 
 export function Badge({
   children,
@@ -34,10 +47,10 @@ export function Badge({
   }
 
   return (
-    <span className={clsx(styles.container, className)}>
+    <span className={container({ className })}>
       {children}
       <span
-        className={clsx(styles.dot, content ? styles.visibleIn : styles.visibleOut, dotClassName)}
+        className={dot({ intent: content ? 'in' : 'out', className: dotClassName })}
         style={{
           backgroundColor: color
         }}
