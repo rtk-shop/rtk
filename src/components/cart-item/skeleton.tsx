7b6594c878232f7d8002edcb7c35f828c53abaf8
fresonn <vladislavs319@gmail.com>
2024-10-19 17:54:55 +0300

    cart-item: structure layout fix

--- src/components/cart-item/skeleton.tsx
+++ src/components/cart-item/skeleton.tsx
@@ -2,7 +2,7 @@ import ContentLoader from 'react-content-loader'
 
 export function CartItemSkeleton() {
   return (
-    <li style={{ padding: '15px 10px 0 10px' }}>
+    <li className="px-2.5 pt-4">
       <ContentLoader backgroundColor="#eeeeee" foregroundColor="#e1e1e1" width="100%">
         <rect x="0" y="0" rx="8" ry="8" width="150" height="150" />
         <rect x="170" y="10" rx="8" ry="8" width="190" height="25" />
