1e73033b97d38db13ecd0049f342c490f3775415
fresonn <vladislavs319@gmail.com>
2024-10-19 13:46:42 +0300

    lang switcher: re-styled with TW

--- src/components/lang-switcher/index.tsx
+++ src/components/lang-switcher/index.tsx
@@ -1,32 +1,50 @@
-import clsx from 'clsx'
+import { cva } from 'cva'
 import { useRouter } from 'next/router'
-import styles from './styles.module.scss'
 
 const locales = {
   ua: 'Ua',
   ru: 'Ru'
 }
 
+type Lang = keyof typeof locales
+
+const langItem = cva('z-20 cursor-pointer select-none px-4 py-1 text-base font-medium', {
+  variants: {
+    selected: {
+      true: 'text-white'
+    }
+  }
+})
+
+const island = cva('w-2/4 h-full z-10 bg-green-400 absolute transition-transform', {
+  variants: {
+    lang: {
+      ru: 'translate-x-full',
+      ua: 'translate-x-0'
+    }
+  }
+})
+
 export function LangSwitcher() {
   const router = useRouter()
-  const { locale = 'ua', pathname, asPath, query } = router
+  const { locale = 'ru', pathname, asPath, query } = router
 
   const handleLangSelect = (lang: string) => {
     router.push({ pathname, query }, asPath, { locale: lang })
   }
 
   return (
-    <div className={styles.container}>
+    <div className="relative flex bg-white">
       {Object.keys(locales).map((lang) => (
         <span
           key={lang}
           onClick={() => handleLangSelect(lang)}
-          className={clsx(styles.lang, lang === locale && styles.current)}
+          className={langItem({ selected: lang === locale })}
         >
-          {locales[lang as keyof typeof locales]}
+          {locales[lang as Lang]}
         </span>
       ))}
-      <div className={clsx(styles.island, locale === 'ua' && styles.ua)} />
+      <div className={island({ lang: locale as Lang })} />
     </div>
   )
 }
