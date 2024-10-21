95835291716927fb95293a550782132db48978fe
fresonn <vladislavs319@gmail.com>
2024-10-21 15:00:29 +0300

    ui: loader fix adaptive styles

--- src/components/ui/loader/index.tsx
+++ src/components/ui/loader/index.tsx
@@ -1,18 +1,16 @@
 import { cva } from 'cva'
 
-const loader = cva(
-  'w-10 aspect-square rounded-full border-6 border-solid border-neutral-200 border-r-black animate-spin',
-  {
-    variants: {
-      adaptive: {
-        true: 'size-auto border-2 border-r-inherit'
-      }
-    },
-    defaultVariants: {
-      adaptive: false
+const loader = cva('aspect-square rounded-full border-solid border-neutral-200 animate-spin', {
+  variants: {
+    adaptive: {
+      true: 'size-auto border-2 border-r-inherit',
+      false: 'w-10 border-6 border-r-black'
     }
+  },
+  defaultVariants: {
+    adaptive: false
   }
-)
+})
 
 export function Loader({ adaptive }: { adaptive?: boolean }) {
   return <div className={loader({ adaptive })} />
