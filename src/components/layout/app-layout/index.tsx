4601b1efd2df79d1652c1d790009b929176e6143
fresonn <vladislavs319@gmail.com>
2024-10-27 12:53:04 +0200

    app-layout(pages) clear

--- src/components/layout/app-layout/index.tsx
+++ src/components/layout/app-layout/index.tsx
@@ -1,12 +1,8 @@
-import { useState, useEffect, useCallback, ReactNode } from 'react'
-import { Navigation } from '../navigation'
-import { Sidebar } from '@/components/layout/sidebar'
-import { Cart } from '../cart'
+import { useEffect, ReactNode } from 'react'
 import { useCartStore } from '@/store/cart'
 import { useFavoriteStore } from '@/store/favorite'
 import { useUiStore } from '@/store/ui'
 import { useUserStore } from '@/store/user'
-import { useGlobalDataQuery } from '@/graphql/global/_gen_/globalData.query'
 
 interface AppLayoutProps {
   children: ReactNode
@@ -14,17 +10,6 @@ interface AppLayoutProps {
 }
 
 export function AppLayout({ children }: AppLayoutProps) {
-  const [isSidebarOpen, setSidebarOpen] = useState(false)
-  const [isCartOpen, setCartOpen] = useState(false)
-
-  const { data, error } = useGlobalDataQuery()
-
-  const currency = data?.globalData.usdCourse || 0
-
-  if (error) {
-    console.log(error.message)
-  }
-
   useEffect(() => {
     useCartStore.persist.rehydrate()
     useFavoriteStore.persist.rehydrate()
@@ -32,28 +17,5 @@ export function AppLayout({ children }: AppLayoutProps) {
     useUserStore.persist.rehydrate()
   }, [])
 
-  const handleOpenDrawer = useCallback(() => {
-    setSidebarOpen(true)
-  }, [])
-
-  const handleCloseDrawer = useCallback(() => {
-    setSidebarOpen(false)
-  }, [])
-
-  const handleCartOpen = useCallback(() => {
-    setCartOpen(true)
-  }, [])
-
-  const handleCartClose = useCallback(() => {
-    setCartOpen(false)
-  }, [])
-
-  return (
-    <>
-      <Sidebar isOpen={isSidebarOpen} currency={currency} onClose={handleCloseDrawer} />
-      <Cart isOpen={isCartOpen} currency={currency} onClose={handleCartClose} />
-      <Navigation onCartOpen={handleCartOpen} onSidebarOpen={handleOpenDrawer} />
-      <main>{children}</main>
-    </>
-  )
+  return <main>{children}</main>
 }
