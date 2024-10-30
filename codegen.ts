bf4e52fb21911afcc065ce79a9345d38d696346f
fresonn <vladislavs319@gmail.com>
2024-10-30 15:55:03 +0200

    codegen.ts lint

--- codegen.ts
+++ codegen.ts
@@ -3,7 +3,6 @@ import { CodegenConfig } from '@graphql-codegen/cli'
 const config: CodegenConfig = {
   overwrite: true,
   schema: 'http://localhost:8080/graphql',
-  // documents: './src/**/*.gql',
   documents: './src/lib/api/graphql/**/*.gql',
   ignoreNoDocuments: true,
   generates: {
