c1b1cf917a39f290acd1af6fee7124c8d015ecc0
fresonn <vladislavs319@gmail.com>
2024-10-31 12:21:03 +0200

    product: added "preview" section

--- src/app/(bot)/product/[id]/page.tsx
+++ src/app/(bot)/product/[id]/page.tsx
@@ -5,8 +5,6 @@ import { notFound } from 'next/navigation'
 import { getProduct } from '@/lib/api'
 import 'keen-slider/keen-slider.min.css'
 
-import styles from '@/components/views/product/styles.module.scss'
-
 export default async function Product({ params }: { params: Promise<{ id: string }> }) {
   const id = (await params).id
 
@@ -16,38 +14,26 @@ export default async function Product({ params }: { params: Promise<{ id: string
     return notFound()
   }
 
-  //   return (
-  //     <div>
-  //       <h1>Product page </h1>
-  //       <h2>Product ID{id}</h2>
-  //       <textarea rows={10} cols={50}>
-  //         {JSON.stringify(data)}
-  //       </textarea>
-  //     </div>
-  //   )
+  if (!(data?.product.__typename === 'Product')) {
+    return notFound()
+  }
 
   const TODO_TAGS: string[] = []
 
   const product = data?.product
 
   return (
-    <div className={styles.container}>
-      <div className={styles.inner}>
-        <div className={styles.previewWrapper}>
-          <Preview images={product.images} />
-        </div>
-        <div className={styles.detailsWrapper}>
-          <Details
-            id={product.id}
-            sku={product.sku}
-            title={product.title}
-            tags={TODO_TAGS}
-            inStock={product.inStock}
-            basePrice={product.basePrice}
-            currentPrice={product.currentPrice}
-          />
-        </div>
-      </div>
+    <div className="mb-16 px-1 pt-1.5">
+      <Preview images={product.images} />
+      <Details
+        id={product.id}
+        sku={product.sku}
+        title={product.title}
+        tags={TODO_TAGS}
+        inStock={product.inStock}
+        basePrice={product.basePrice}
+        currentPrice={product.currentPrice}
+      />
       {/* <Info
         gender={product.gender}
         description={product.description || ''}
--- src/app/(bot)/product/[id]/preview/index.tsx
+++ src/app/(bot)/product/[id]/preview/index.tsx
@@ -1,41 +1,28 @@
 'use client'
 
 import { useState } from 'react'
-import clsx from 'clsx'
 import { Thumbs } from './thumbs'
-import { NavButtons } from '@/components/ui/nav-buttons'
 import { ImagePlaceholder } from '@/components/ui/image-placeholder'
 import { useKeenSlider } from 'keen-slider/react'
-
-import styles from './styles.module.scss'
 
 interface PreviewProps {
   images: string[]
 }
 
 export function Preview({ images }: PreviewProps) {
-  const [currentIndex, setCurrentIndex] = useState<number>(0)
+  const [currentIndex, setCurrentIndex] = useState(0)
 
   const [sliderRef, instanceRef] = useKeenSlider({
     initial: 0,
     loop: true,
     slides: {
-      spacing: 10,
-      perView: 'auto',
+      perView: 1,
       number: images.length
     },
     slideChanged(slider) {
       setCurrentIndex(slider.track.details.rel)
     }
   })
-
-  const handlePrevClick = () => {
-    instanceRef.current?.prev()
-  }
-
-  const handleNextClick = () => {
-    instanceRef.current?.next()
-  }
 
   const handlePaginationChange = (index: number): void => {
     if (instanceRef.current) {
@@ -44,19 +31,15 @@ export function Preview({ images }: PreviewProps) {
   }
 
   return (
-    <div className={styles.root}>
+    <div>
+      <ul ref={sliderRef} className="keen-slider cursor-grab">
+        {images.map((image, index) => (
+          <li key={index} className="keen-slider__slide">
+            <ImagePlaceholder priority src={image} altText={`фото №${index + 1}`} />
+          </li>
+        ))}
+      </ul>
       <Thumbs activeIndex={currentIndex} images={images} onChange={handlePaginationChange} />
-
-      <div className={styles.sliderWrapper}>
-        <ul ref={sliderRef} className={clsx('keen-slider', styles.previewSlider)}>
-          {images.map((image, index) => (
-            <li key={index} className="keen-slider__slide">
-              <ImagePlaceholder priority src={image} altText={`фото №${index + 1}`} />
-            </li>
-          ))}
-        </ul>
-        <NavButtons onPrev={handlePrevClick} onNext={handleNextClick} />
-      </div>
     </div>
   )
 }
