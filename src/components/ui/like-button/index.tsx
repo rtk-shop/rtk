929008193992fa94380d583dd4b1366826b340cd
fresonn <vladislavs319@gmail.com>
2024-11-01 20:02:22 +0200

    ui: like button update

--- src/components/ui/like-button/index.tsx
+++ src/components/ui/like-button/index.tsx
@@ -1,59 +1,31 @@
 import { MouseEvent } from 'react'
-import clsx from 'clsx'
 import { IconButton } from '@/components/ui/icon-button'
 
-import styles from './styles.module.scss'
+import styles from './styles.module.css'
 
 interface LikeButtonProps {
   liked: boolean
   width?: number
   height?: number
-  text?: string
   disableRipple?: boolean
   className?: string
   onClick: (e: MouseEvent) => void
 }
 
-export function LikeButton({
-  liked,
-  width = 20,
-  height = 20,
-  text,
-  className,
-  ...restProps
-}: LikeButtonProps) {
+export function LikeButton({ liked, width = 20, height = 20, ...restProps }: LikeButtonProps) {
   return (
-    <IconButton className={clsx(styles.button, className)} {...restProps}>
+    <IconButton className={styles.button} {...restProps}>
       <svg
         viewBox="0 0 24 24"
         width={width}
         height={height}
-        className={clsx({
-          [styles.svgEL]: true,
-          [styles.liked]: liked,
-          [styles.withText]: !!text
-        })}
+        className={styles.svgEL + ' ' + (liked ? styles.liked : '')}
       >
-        <use xlinkHref="#heart" />
-        <use xlinkHref="#heart" />
+        <path
+          id="heart"
+          d="M12 4.435c-1.989-5.399-12-4.597-12 3.568 0 4.068 3.06 9.481 12 14.997 8.94-5.516 12-10.929 12-14.997 0-8.118-10-8.999-12-3.568z"
+        />
       </svg>
-      <svg
-        viewBox="0 0 24 24"
-        width={width}
-        height={height}
-        className={clsx({
-          [styles.hide]: true,
-          [styles.liked]: liked
-        })}
-      >
-        <defs>
-          <path
-            id="heart"
-            d="M12 4.435c-1.989-5.399-12-4.597-12 3.568 0 4.068 3.06 9.481 12 14.997 8.94-5.516 12-10.929 12-14.997 0-8.118-10-8.999-12-3.568z"
-          />
-        </defs>
-      </svg>
-      {text && <span>{text}</span>}
     </IconButton>
   )
 }
