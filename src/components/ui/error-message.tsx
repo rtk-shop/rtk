aadc5b146e0178e1364de6b3ae319981472d6ec5
fresonn <vladislavs319@gmail.com>
2024-11-07 13:08:12 +0200

    new UI component: error-message

---
+++ src/components/ui/error-message.tsx
@@ -0,0 +1,14 @@
+import { type ReactNode } from 'react'
+import { cva } from 'cva'
+
+const msg = cva('h-6 pl-2.5 text-[13px] font-medium leading-none text-red-600', {
+  variants: {
+    show: {
+      true: 'duration-300 animate-in fade-in'
+    }
+  }
+})
+
+export function ErrorMessage({ show, children }: { show: boolean; children: ReactNode }) {
+  return <p className={msg({ show })}>{children}</p>
+}
