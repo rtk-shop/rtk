f99fb6ae758916c5a3bcb63deb2839574d2db1b8
fresonn <vladislavs319@gmail.com>
2024-11-10 18:29:00 +0200

    ui: error message - select none

--- src/components/ui/error-message.tsx
+++ src/components/ui/error-message.tsx
@@ -1,7 +1,7 @@
 import { type ReactNode } from 'react'
 import { cva } from 'cva'
 
-const msg = cva('h-6 pl-2.5 text-[13px] font-medium leading-none text-red-600', {
+const msg = cva('h-6 select-none pl-2.5 text-[13px] font-medium leading-none text-red-600', {
   variants: {
     show: {
       true: 'duration-300 animate-in fade-in'
