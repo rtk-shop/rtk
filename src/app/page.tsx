b9177273d57f5a38fd942d58ad81fceacf3534eb
fresonn <vladislavs319@gmail.com>
2024-11-10 17:16:19 +0200

    root app page for tests, temporary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
 export default function Page() {
-  return <h1>Rooot Page</h1>
+  return (
+    <div>
+      <h1>Tests</h1>
+      <div className="p-10">
+        <button className="duration-700 animate-in fade-in">Button A</button>
+        <button className="... animate-in spin-in">Button B</button>
+        <button className="... animate-in zoom-in">Button C</button>
+        <button className="... animate-in slide-in-from-top">Button D</button>
+        <button className="... animate-in slide-in-from-left">Button E</button>
+        <br />
+      </div>
+    </div>
+  )
 }
