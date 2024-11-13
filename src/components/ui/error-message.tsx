13093c6632335a9510c5b17e9d7f90fb87beff5f
fresonn <vladislavs319@gmail.com>
2024-11-13 16:10:40 +0200

    ui: error message - fix styles

--- src/components/ui/error-message.tsx
+++ src/components/ui/error-message.tsx
@@ -4,7 +4,8 @@ import { cva } from 'cva'
 const msg = cva('h-6 select-none pl-2.5 text-[13px] font-medium leading-none text-red-600', {
   variants: {
     show: {
-      true: 'duration-300 animate-in fade-in'
+      true: 'visible duration-300 animate-in fade-in',
+      false: 'invisible'
     }
   }
 })
