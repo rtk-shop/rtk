bd220269d3fdfe33110a6b179e1af530210a01c4
fresonn <vladislavs319@gmail.com>
2024-12-02 14:01:09 +0200

    lang-swither: styles update

--- src/components/lang-switcher/index.tsx
+++ src/components/lang-switcher/index.tsx
@@ -3,15 +3,7 @@ import { setUserLocale } from '@/lib/locale'
 import { useLocale } from 'next-intl'
 import { locales, type Locale } from '@/i18n/config'
 
-const langItem = cva('z-20 cursor-pointer select-none px-4 py-1 text-base font-medium', {
-  variants: {
-    selected: {
-      true: 'text-white'
-    }
-  }
-})
-
-const island = cva('absolute z-10 h-full w-2/4 bg-green-400 transition-transform', {
+const island = cva('absolute z-10 h-full w-2/4 bg-green transition-transform', {
   variants: {
     lang: {
       ua: 'translate-x-full',
@@ -38,7 +30,7 @@ export function LangSwitcher() {
         <div
           key={lang}
           onClick={() => handleLangSelect(lang)}
-          className={langItem({ selected: lang === locale })}
+          className="z-20 cursor-pointer select-none px-4 py-1 text-base font-medium"
         >
           {view[lang]}
         </div>
