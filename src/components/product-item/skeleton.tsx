841eab22a98c1c3db76b0e16e383362bcb53a95c
fresonn <vladislavs319@gmail.com>
2024-12-16 12:10:55 +0200

    product item: skeleton update (4:5)

--- src/components/product-item/skeleton.tsx
+++ src/components/product-item/skeleton.tsx
@@ -9,12 +9,12 @@ export function Skeleton() {
       backgroundColor="#eee"
       foregroundColor="#e1e1e1"
       width="100%"
-      viewBox="0 0 284 380"
+      viewBox="0 0 290 410"
     >
-      <rect x="0" y="0" rx="8" ry="8" width="100%" height="70%" />
-      <rect x="0" y="74%" rx="6" ry="6" width="69" height="22" />
-      <circle cx="90%" cy="76%" r="17" />
-      <rect x="0" y="84%" rx="6" ry="6" width="100%" height="27" />
+      <rect x="0" y="0" rx="8" ry="8" width="100%" height="80%" />
+      <rect x="0" y="84%" rx="6" ry="6" width="74" height="22" />
+      <circle cx="91%" cy="86%" r="17" />
+      <rect x="0" y="93%" rx="6" ry="6" width="100%" height="27" />
     </ContentLoader>
   )
 }
