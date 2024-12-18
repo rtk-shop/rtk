6a08d96450f774bc5dd38c2d27ee763092f1bc14
fresonn <vladislavs319@gmail.com>
2024-12-18 14:26:05 +0200

    [FIX] product item skeleton SSR uniqueKey error

--- src/components/product-item/skeleton.tsx
+++ src/components/product-item/skeleton.tsx
@@ -1,6 +1,9 @@
+import { useId } from 'react'
 import ContentLoader from 'react-content-loader'
 
 export function Skeleton() {
+  const uniqueId = useId()
+
   return (
     <ContentLoader
       style={{
@@ -10,6 +13,7 @@ export function Skeleton() {
       foregroundColor="#e1e1e1"
       width="100%"
       viewBox="0 0 290 410"
+      uniqueKey={uniqueId} // https://github.com/danilowoz/react-content-loader?tab=readme-ov-file#server-side-rendering-ssr---match-snapshot
     >
       <rect x="0" y="0" rx="8" ry="8" width="100%" height="80%" />
       <rect x="0" y="84%" rx="6" ry="6" width="74" height="22" />
