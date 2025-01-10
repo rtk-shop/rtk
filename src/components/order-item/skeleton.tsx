5acad71d0e917b906dcf1bfd35af8e9cc7703acf
fresonn <vladislavs319@gmail.com>
2025-01-10 15:25:15 +0200

    order item: static skeleton

--- src/components/order-item/skeleton.tsx
+++ src/components/order-item/skeleton.tsx
@@ -10,14 +10,14 @@ export function OrderItemSkeleton() {
       backgroundColor="#e5e7eb"
       foregroundColor="#f3f4f6"
       width="100%"
-      hanging={48}
+      height="46px"
       speed={1}
-      viewBox="0 0 380 46"
+      viewBox="0 0 580 46"
       uniqueKey={uniqueId} // https://github.com/danilowoz/react-content-loader?tab=readme-ov-file#server-side-rendering-ssr---match-snapshot
     >
       <rect x="2%" y="28%" rx="4" ry="4" width="40" height="20" />
-      <rect x="25%" y="28%" rx="4" ry="4" width="91" height="20" />
-      <rect x="61%" y="28%" rx="4" ry="4" width="70" height="20" />
+      <rect x="26%" y="28%" rx="4" ry="4" width="95" height="20" />
+      <rect x="62%" y="28%" rx="4" ry="4" width="75" height="20" />
       <circle cx="94%" cy="23" r="13" />
     </ContentLoader>
   )
