b9aac480dc2f893fea39612db93c8246106c15d9
fresonn <vladislavs319@gmail.com>
2025-01-10 15:12:37 +0200

    ui: scroll-mask got "vertical" direction

--- src/components/ui/scroll-mask.tsx
+++ src/components/ui/scroll-mask.tsx
@@ -1,7 +1,7 @@
 import { cva } from 'cva'
 import { useRef, useState, type ReactNode } from 'react'
 
-const container = cva('no-scrollbar overflow-x-auto', {
+const container = cva('no-scrollbar h-[inherit] overflow-auto', {
   variants: {
     showRightMask: {
       true: 'gradient-mask-r-10',
@@ -10,13 +10,24 @@ const container = cva('no-scrollbar overflow-x-auto', {
     showLeftMask: {
       true: 'gradient-mask-l-70',
       false: ''
+    },
+    showBottomMask: {
+      true: 'gradient-mask-b-60',
+      false: ''
     }
   }
 })
 
 // DOCS: https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollLeft
+//       https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollTop
 
-export function ScrollMask({ children }: { children: ReactNode }) {
+export function ScrollMask({
+  direction = 'horizontal',
+  children
+}: {
+  direction?: 'vertical' | 'horizontal'
+  children: ReactNode
+}) {
   const scrollDemoRef = useRef(null)
 
   const [scrollPosition, setScrollPosition] = useState({ scrollTop: 0, scrollLeft: 0 })
@@ -32,8 +43,9 @@ export function ScrollMask({ children }: { children: ReactNode }) {
     <div
       ref={scrollDemoRef}
       className={container({
-        showLeftMask: scrollPosition.scrollLeft > 0,
-        showRightMask: scrollPosition.scrollLeft === 0
+        showLeftMask: direction === 'horizontal' && scrollPosition.scrollLeft > 0,
+        showRightMask: direction === 'horizontal' && scrollPosition.scrollLeft === 0,
+        showBottomMask: direction === 'vertical' && scrollPosition.scrollTop === 0
       })}
       onScroll={handleScroll}
     >
