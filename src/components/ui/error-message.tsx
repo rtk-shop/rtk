6c24d824cde4f07afe7970ba4e77666875c61903
fresonn <vladislavs319@gmail.com>
2025-01-12 19:00:14 +0200

    ui: error-message got "align" prop

--- src/components/ui/error-message.tsx
+++ src/components/ui/error-message.tsx
@@ -6,10 +6,23 @@ const msg = cva('h-6 select-none pl-2.5 text-[13px] font-medium leading-none tex
     show: {
       true: 'visible duration-300 animate-in fade-in',
       false: 'invisible'
+    },
+    align: {
+      start: 'text-start',
+      center: 'text-center',
+      end: 'text-end'
     }
   }
 })
 
-export function ErrorMessage({ show, children }: { show: boolean; children: ReactNode }) {
-  return <p className={msg({ show })}>{children}</p>
+export function ErrorMessage({
+  show,
+  align = 'start',
+  children
+}: {
+  show: boolean
+  align?: 'start' | 'center' | 'end'
+  children: ReactNode
+}) {
+  return <p className={msg({ show, align })}>{children}</p>
 }
