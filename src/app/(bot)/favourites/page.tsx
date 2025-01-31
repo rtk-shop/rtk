7146f3a6ac2c5d6f3586f7f6dbf051eb074cfb0b
fresonn <vladislavs319@gmail.com>
2025-01-31 21:00:47 +0200

    /favourites: products full screen

--- src/app/(bot)/favourites/page.tsx
+++ src/app/(bot)/favourites/page.tsx
@@ -7,7 +7,7 @@ export default function Favourites() {
       <h1 className="text-2xl leading-0 font-medium">
         Понравившиеся товары <Icon name="common/heart" className="text-red-600" />
       </h1>
-      <div className="pt-4">
+      <div className="h-full pt-4">
         <Products />
       </div>
     </div>
