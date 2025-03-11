fb8df0a491104b83fa2ec44d9ee1a580e88465ab
fresonn <vladislavs319@gmail.com>
2025-03-11 15:20:14 +0200

    sidebar: categories view update

--- src/components/layout/sidebar/categories.tsx
+++ src/components/layout/sidebar/categories.tsx
@@ -1,4 +1,3 @@
-import { cva } from 'cva'
 import Link from 'next/link'
 import Image from 'next/image'
 import { routeNames } from '@/lib/constants'
@@ -8,17 +7,8 @@ const categoriesValues: Array<{
   img: string
   to: string
   i18n: string
+  sub?: string[]
 }> = [
-  {
-    img: '/assets/bag.webp',
-    to: routeNames.catalog,
-    i18n: 'bags'
-  },
-  {
-    img: '/assets/wallet.webp',
-    to: routeNames.catalog,
-    i18n: 'wallets'
-  },
   {
     img: '/assets/backpack.webp',
     to: routeNames.catalog,
@@ -27,25 +17,66 @@ const categoriesValues: Array<{
   {
     img: '/assets/suitcase.webp',
     to: routeNames.catalog,
-    i18n: 'suitcases'
+    i18n: 'suitcases',
+    sub: ['nouns.polypropylene', 'nouns.textile', 'nouns.covers']
+  },
+  {
+    img: '/assets/bag.webp',
+    to: routeNames.catalog,
+    i18n: 'bags',
+    sub: ['nouns.sling', 'actions.forDocs', 'actions.forLaptop']
   }
 ]
-
-const pseudoLine = cva('absolute left-1/2 top-2/4 bg-gray-400', {
-  variants: {
-    type: {
-      vertical: 'h-full w-px -translate-y-2/4',
-      horizontal: 'h-px w-full -translate-x-2/4'
-    }
-  }
-})
 
 export function Categories() {
   const t = useTranslations('Common')
 
   return (
-    <section className="px-4">
-      <ul className="relative flex flex-wrap rounded-md border border-gray-400 bg-white/10 text-gray-100">
+    <section className="">
+      <ul className="relative flex flex-col">
+        {categoriesValues.map((category, ind) => (
+          <li key={ind} className="mb-2.5 flex">
+            <Link href={category.to} className="flex items-center">
+              <div className="size-36">
+                <Image
+                  priority
+                  src={category.img}
+                  quality={100}
+                  width={144}
+                  height={144}
+                  alt={`Изображение категории - ${t(`categories.${category.i18n}`)}`}
+                />
+              </div>
+              <div className="pt-0 font-medium text-gray-100">
+                <p className="mb-1 text-2xl font-medium">{t(`categories.${category.i18n}`)}</p>
+                <ul className="list-inside pl-0.5 text-sm">
+                  {category.sub &&
+                    category.sub.map((el, index) => (
+                      <li key={index}>
+                        <li key={index}>- {t(el)}</li>
+                      </li>
+                    ))}
+                  <li className="list-none text-gray-300 underline">{t('actions.showAll')}</li>
+                </ul>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
+}
+
+// const pseudoLine = cva('absolute top-2/4 left-1/2 bg-gray-400', {
+//   variants: {
+//     type: {
+//       vertical: 'h-full w-px -translate-y-2/4',
+//       horizontal: 'h-px w-full -translate-x-2/4'
+//     }
+//   }
+// })
+
+/* <ul className="relative flex flex-wrap rounded-md border border-gray-400 bg-white/10 text-gray-100">
         {categoriesValues.map((category, ind) => (
           <li key={ind} className="mb-2.5 flex basis-1/2 justify-center">
             <Link href={category.to} className="items-center">
@@ -67,7 +98,4 @@ export function Categories() {
         ))}
         <div className={pseudoLine({ type: 'vertical' })} />
         <div className={pseudoLine({ type: 'horizontal' })} />
-      </ul>
-    </section>
-  )
-}
+      </ul> */
