e14d449997beb926b43db159f54f54f68f6d0c92
fresonn <vladislavs319@gmail.com>
2025-03-13 12:03:33 +0200

    tailwind update | some styles refactoring

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,17 +7,11 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
-      spacing: {
-        '1e': '1em'
-      },
       backgroundImage: {
         'cap-pattern':
           'linear-gradient(90deg, #fa8bff 0%, #2bff88 26%, #000000 65%, #000000 90%, #2bff88 99%)',
         suitcases: "url('/assets/suitcases.jpeg')",
         checkoutPromo: "url('/assets/balloons-pattern.jpg')"
-      },
-      borderWidth: {
-        6: '6px'
       },
       transitionProperty: {
         expand: 'height, opacity'
