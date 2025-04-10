3996c1b75267eaa5cc70d1a49acd1b7d2cf5500d
fresonn <vladislavs319@gmail.com>
2025-04-10 14:45:45 +0300

    uI: prettier setup

--- packages/ui/src/badge.tsx
+++ packages/ui/src/badge.tsx
@@ -1,46 +1,40 @@
-import { ReactNode } from "react";
-import { cva } from "cva";
+import { ReactNode } from 'react'
+import { cva } from 'cva'
 
 export interface BadgeProps {
-  max?: number;
-  children: ReactNode;
-  content?: string | number;
-  className?: string;
-  dotClassName?: string;
+  max?: number
+  children: ReactNode
+  content?: string | number
+  className?: string
+  dotClassName?: string
 }
 
-const container = cva("relative inline-flex align-middle");
+const container = cva('relative inline-flex align-middle')
 
 const dot = cva(
-  "absolute right-0 top-0 z-10 flex size-5 origin-center flex-row flex-wrap content-center items-center justify-center rounded-full bg-red-500 px-1.5 text-xs font-medium leading-none text-white transition-transform",
+  'absolute top-0 right-0 z-10 flex size-5 origin-center flex-row flex-wrap content-center items-center justify-center rounded-full bg-red-500 px-1.5 text-xs leading-none font-medium text-white transition-transform',
   {
     variants: {
       intent: {
-        in: ["scale-100", "translate-x-2/4", "-translate-y-2/4"],
-        out: ["scale-0", "translate-x-2/4", "-translate-y-2/4"],
-      },
-    },
+        in: ['scale-100', 'translate-x-2/4', '-translate-y-2/4'],
+        out: ['scale-0', 'translate-x-2/4', '-translate-y-2/4']
+      }
+    }
   }
-);
+)
 
-export function Badge({
-  children,
-  content,
-  max = 100,
-  className,
-  dotClassName,
-}: BadgeProps) {
-  let show = "";
+export function Badge({ children, content, max = 100, className, dotClassName }: BadgeProps) {
+  let show = ''
 
-  if (typeof content === "number") {
-    show = String(content);
+  if (typeof content === 'number') {
+    show = String(content)
     if (content > max) {
-      show = max + "+";
+      show = max + '+'
     }
   } else {
-    show = content || "";
+    show = content || ''
     if (show.length > max) {
-      show = max + "+";
+      show = max + '+'
     }
   }
 
@@ -49,12 +43,12 @@ export function Badge({
       {children}
       <span
         className={dot({
-          intent: content ? "in" : "out",
-          className: dotClassName,
+          intent: content ? 'in' : 'out',
+          className: dotClassName
         })}
       >
         {show}
       </span>
     </div>
-  );
+  )
 }
