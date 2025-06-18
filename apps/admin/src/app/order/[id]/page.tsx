6bd952d3d8200dc8f6b3c400b369bdd869ff07ff
fresonn <vladislavs319@gmail.com>
2025-06-18 19:32:13 +0300

    admin: /order - createdAt, updatedAt sections

--- apps/admin/src/app/order/[id]/page.tsx
+++ apps/admin/src/app/order/[id]/page.tsx
@@ -3,6 +3,8 @@ import { getOrder } from '@/app/actions'
 import { Receiver } from './receiver'
 import { Delivery } from './delivery'
 import { notFound } from 'next/navigation'
+import { formatDate } from '@repo/utils'
+import { StatusBadge } from '@/components/order/status-badge'
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params
@@ -22,13 +24,21 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
   if (order.__typename === 'NotFound') notFound()
 
   return (
-    <div>
-      <div>Order: {id}</div>
-      <Link className="bg-gray-300" href="/dashboard">
-        /dashboard
-      </Link>
+    <div className="p-2.5">
+      <div className="mb-2.5 flex items-center">
+        <h1 className="mr-2.5 text-xl font-medium">Заказ №{order.id}</h1>
+        <StatusBadge status={order.status} />
+      </div>
+      <p>
+        <span className="font-medium">Создан: </span>&nbsp;
+        {formatDate(order.createdAt, { dateStyle: 'short', timeStyle: 'short' })}
+      </p>
+      <p>
+        <span className="font-medium">Обновлен: </span>&nbsp;
+        {formatDate(order.updatedAt, { dateStyle: 'short', timeStyle: 'short' })}
+      </p>
 
-      <div className="flex flex-col p-2 sm:flex-row">
+      <div className="flex flex-col py-2 sm:flex-row">
         <Receiver
           name={order.receiverName}
           surname={order.receiverSurname}
@@ -41,6 +51,9 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
           parcelTrackId={order.parcelTrackId}
         />
       </div>
+      <Link className="bg-gray-300" href="/dashboard">
+        /dashboard
+      </Link>
     </div>
   )
 }
