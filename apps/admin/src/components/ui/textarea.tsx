63474e5e5a7987dbe148f47c475fd67c39b0a6ab
fresonn <vladislavs319@gmail.com>
2025-06-25 14:41:41 +0300

    admin: new ui component — textarea

---
+++ apps/admin/src/components/ui/textarea.tsx
@@ -0,0 +1,38 @@
+import { useId, type ReactNode } from 'react'
+import { Textarea as TextareaRoot } from '@/components/ui/shadcn/textarea'
+import { useFormContext, Controller } from 'react-hook-form'
+import { useTranslations } from 'next-intl'
+import { ErrorMessage } from '@repo/ui'
+import { Label } from './label'
+
+export type TextareaProps = Omit<React.ComponentProps<'textarea'>, 'name'> & {
+  name: string
+  label: ReactNode
+}
+
+export function Textarea({ name, label, disabled, defaultValue, ...otherProps }: TextareaProps) {
+  const t = useTranslations()
+  const uniqueId = useId()
+
+  const {
+    control,
+    formState: { errors }
+  } = useFormContext()
+
+  const isErr = !!(errors && errors[name])
+  const message = isErr && t(errors[name]?.message as string)
+
+  return (
+    <>
+      {label && <Label htmlFor={uniqueId}>{label}</Label>}
+      <Controller
+        name={name}
+        control={control}
+        disabled={disabled}
+        defaultValue={defaultValue}
+        render={({ field }) => <TextareaRoot {...field} {...otherProps} id={uniqueId} />}
+      />
+      <ErrorMessage show={isErr}>{message}</ErrorMessage>
+    </>
+  )
+}
