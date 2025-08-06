6649e9dd16f860e090b4107ec39b0707872b56d4
fresonn <vladislavs319@gmail.com>
2025-08-06 14:42:06 +0300

    app: refactor(ui): remove sidebar footer info

--- apps/tg-web-app/src/components/layout/sidebar/index.tsx
+++ apps/tg-web-app/src/components/layout/sidebar/index.tsx
@@ -4,11 +4,8 @@ import { Drawer } from '@/components/ui/drawer'
 import { SidebarHead } from './head'
 import { Categories } from './categories'
 import { useAppState } from '@/stores/app/store'
-// import { useTranslations } from 'next-intl'
 
 export function Sidebar() {
-  // const t = useTranslations('Common')
-
   const isSidebarOpen = useAppState((state) => state.isSidebarOpen)
   const closeSidebar = useAppState((state) => state.closeSidebar)
 
@@ -26,18 +23,6 @@ export function Sidebar() {
               SANDBOX
             </a>
           )}
-          <div className="flex grow flex-col justify-end"></div>
-          <ul className="p-5 text-sm text-white *:flex *:justify-between">
-            <li>
-              Телефон: <span>{process.env.NEXT_PUBLIC_CONTACT_PHONE}</span>
-            </li>
-            <li>
-              Прием заказов: <span>{process.env.NEXT_PUBLIC_ORDER_ACCEPTANCE_TIME}</span>
-            </li>
-            <li>
-              Дни работы: <span>{process.env.NEXT_PUBLIC_WORKING_DAYS}</span>
-            </li>
-          </ul>
         </div>
       </div>
     </Drawer>
