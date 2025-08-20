b99a0878cf3d37f5fb2203df62fba0dbe61fb54e
fresonn <vladislavs319@gmail.com>
2025-08-20 22:18:59 +0300

    app: feat(ui): add order status badge size variations

--- apps/tg-web-app/src/components/order/status-badge.tsx
+++ apps/tg-web-app/src/components/order/status-badge.tsx
@@ -4,18 +4,43 @@ import { OrderStatus } from '@/lib/api/graphql/types'
 import { Icon } from '@/components/ui/icon'
 import { useTranslations } from 'next-intl'
 
-const iconStyle = cva('mr-0.5 shrink-0 text-[20px]', {
+const statusView = cva('font-medium', {
+  variants: {
+    size: {
+      normal: 'text-[15px]',
+      XL: 'text-lg'
+    }
+  },
+  defaultVariants: {
+    size: 'normal'
+  }
+})
+
+const iconStyle = cva('shrink-0', {
   variants: {
     status: {
       [OrderStatus.Rejected]: 'text-gray-500',
       [OrderStatus.Returned]: 'text-red-500',
       [OrderStatus.Processed]: 'text-black',
       [OrderStatus.Done]: 'text-green-700'
+    },
+    size: {
+      normal: 'mr-0.5 text-[20px]',
+      XL: 'mr-1 text-[24px]'
     }
+  },
+  defaultVariants: {
+    size: 'normal'
   }
 })
 
-export function OrderStatusBadge({ status }: { status: OrderStatus }) {
+export function OrderStatusBadge({
+  status,
+  size
+}: {
+  status: OrderStatus
+  size?: 'normal' | 'XL'
+}) {
   const t = useTranslations('Common.order.statuses')
 
   let statusIcon: ReactNode
@@ -23,7 +48,7 @@ export function OrderStatusBadge({ status }: { status: OrderStatus }) {
   switch (status) {
     case OrderStatus.Created:
       statusIcon = (
-        <span className="relative mr-1 ml-0.5 flex size-2.5">
+        <span className="relative mr-1 ml-0.5 flex size-3 justify-center">
           <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
           <span className="relative inline-flex size-2.5 rounded-full bg-green-500"></span>
         </span>
@@ -31,28 +56,28 @@ export function OrderStatusBadge({ status }: { status: OrderStatus }) {
       break
     case OrderStatus.Sent:
       statusIcon = (
-        <span className="relative mr-1 ml-0.5 flex size-2.5">
-          <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-blue-400 opacity-75"></span>
-          <span className="relative inline-flex size-2.5 rounded-full bg-blue-500"></span>
+        <span className="relative mr-1 ml-0.5 flex size-3 justify-center">
+          <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-blue-400 opacity-75" />
+          <span className="relative inline-flex size-2.5 rounded-full bg-blue-500" />
         </span>
       )
       break
     case OrderStatus.Processed:
-      statusIcon = <Icon name="profile/timer" className={iconStyle({ status })} />
+      statusIcon = <Icon name="profile/timer" className={iconStyle({ status, size })} />
       break
     case OrderStatus.Done:
-      statusIcon = <Icon name="profile/package-check" className={iconStyle({ status })} />
+      statusIcon = <Icon name="profile/package-check" className={iconStyle({ status, size })} />
       break
     case OrderStatus.Rejected:
-      statusIcon = <Icon name="profile/package-x" className={iconStyle({ status })} />
+      statusIcon = <Icon name="profile/package-x" className={iconStyle({ status, size })} />
       break
     case OrderStatus.Returned:
-      statusIcon = <Icon name="profile/package-x" className={iconStyle({ status })} />
+      statusIcon = <Icon name="profile/package-x" className={iconStyle({ status, size })} />
       break
   }
 
   return (
-    <div className="text-[15px] font-medium">
+    <div className={statusView({ size })}>
       <div className="flex items-center">
         {statusIcon}
         <span>{t(status.toLowerCase())}</span>
