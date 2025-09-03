f8841a51ca9a040ce9f9a8f40111359e973e2705
fresonn <vladislavs319@gmail.com>
2025-09-03 21:36:28 +0300

    app: fix(ui): fix order status badge dynamic  dots

--- apps/tg-web-app/src/components/order/status-badge.tsx
+++ apps/tg-web-app/src/components/order/status-badge.tsx
@@ -48,7 +48,7 @@ export function OrderStatusBadge({
   switch (status) {
     case OrderStatus.Created:
       statusIcon = (
-        <span className="relative mr-1 ml-0.5 flex size-3 justify-center">
+        <span className="relative mr-1 mb-0.5 ml-0.5 flex size-2.5 justify-center">
           <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
           <span className="relative inline-flex size-2.5 rounded-full bg-green-500"></span>
         </span>
@@ -56,7 +56,7 @@ export function OrderStatusBadge({
       break
     case OrderStatus.Sent:
       statusIcon = (
-        <span className="relative mr-1 ml-0.5 flex size-3 justify-center">
+        <span className="relative mr-1 mb-0.5 ml-0.5 flex size-2.5 justify-center">
           <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-blue-400 opacity-75" />
           <span className="relative inline-flex size-2.5 rounded-full bg-blue-500" />
         </span>
