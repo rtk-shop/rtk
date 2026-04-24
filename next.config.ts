fdd80a8e3bc342f50c5ff618fff75238592e11b5
fresonn <vladislavs319@gmail.com>
2026-04-24 20:09:45 +0300

    Add analyzer

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,21 @@
 import type { NextConfig } from 'next'
 import createNextIntlPlugin from 'next-intl/plugin'
+import bundleAnalyzer from '@next/bundle-analyzer'
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true'
+})
 
 const withNextIntl = createNextIntlPlugin()
 
 const nextConfig: NextConfig = {
   reactCompiler: false,
   reactStrictMode: true,
+
   experimental: {
     optimizePackageImports: ['urql', '@urql/exchange-auth']
   },
+
   images: {
     unoptimized: true,
     remotePatterns: [
@@ -20,4 +27,4 @@ const nextConfig: NextConfig = {
   }
 }
 
-export default withNextIntl(nextConfig)
+export default withNextIntl(withBundleAnalyzer(nextConfig))
