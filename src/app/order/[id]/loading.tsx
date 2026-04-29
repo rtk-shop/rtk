5d9784b49ab0a8d7871b071cfc6f5d34e6eda6ba
fresonn <vladislavs319@gmail.com>
2026-04-29 12:30:12 +0300

    update loading view

--- src/app/order/[id]/loading.tsx
+++ src/app/order/[id]/loading.tsx
@@ -1,12 +1,35 @@
 import { Box } from '@/components/ui/box'
-import { Loader } from '@/components/ui/loader'
+import ContentLoader from 'react-content-loader'
 
 export default function Loading() {
+  const round = 10
+
   return (
-    <Box flex="row" align="center" justify="center" className="h-dvh">
-      <Box className="animate-in fade-in zoom-in">
-        <Loader size="lg" />
-      </Box>
+    <Box className="bg-gray-100 px-2">
+      <ContentLoader
+        className=""
+        backgroundColor="#e5e7eb"
+        foregroundColor="#f9fafb"
+        width="100%"
+        viewBox="0 0 600 1200"
+      >
+        <rect x="24%" y="25" rx={2} ry={2} width="300" height="33" />
+        <rect x="30%" y="70" rx={2} ry={2} width="220" height="25" />
+
+        <rect x="0" y="120" rx={round} ry={round} width="100%" height="72" />
+        <rect x="0" y="210" rx={round} ry={round} width="60%" height="150" />
+        <rect x="61%" y="210" rx={round} ry={round} width="39%" height="150" />
+
+        <circle cx="20" cy="405" r="17" />
+        <rect x="50" y="390" rx={4} ry={4} width="70%" height="30" />
+        <rect x="0" y="430" rx={round} ry={round} width="100%" height="150" />
+
+        <circle cx="20" cy="625" r="17" />
+        <rect x="50" y="610" rx={4} ry={4} width="70%" height="30" />
+        <rect x="0" y="650" rx={round} ry={round} width="100%" height="165" />
+
+        <rect x="0" y="850" rx={20} ry={20} width="100%" height="600" />
+      </ContentLoader>
     </Box>
   )
 }
